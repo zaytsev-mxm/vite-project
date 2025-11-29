a1ad9a31057b2352e2e0e1cad559713b94302dd3
Max Zaytsev <zaytsev.mxm@gmail.com>
2025-11-29 19:41:15 +0000

    added all possible test cases for curry with placeholder

--- src/bigfrontend/javascript/02-implement-curry-with-placeholder/02-implement-curry-with-placeholder.test.ts
+++ src/bigfrontend/javascript/02-implement-curry-with-placeholder/02-implement-curry-with-placeholder.test.ts
@@ -8,8 +8,28 @@ function join(a: any, b: any, c: any) {
 
 const _ = curry.placeholder;
 
-describe('join with placeholder', () => {
-  test('it works with placeholder', () => {
+describe('curry with placeholder', () => {
+  test('test #1', () => {
+    expect(curry(join)(1)(2)(3)).toBe('1_2_3');
+  });
+  test('test #2', () => {
+    expect(curry(join)(1, 2)(3)).toBe('1_2_3');
+  });
+  test('test #3', () => {
+    expect(curry(join)(1, 2, 3, 4)).toBe('1_2_3');
+  });
+  test('test #4', () => {
+    const curried = curry(join)(1, 2);
+    expect(curried(3)).toBe('1_2_3');
+    expect(curried(4)).toBe('1_2_4');
+  });
+  test('test #5', () => {
     expect(curry(join)(_, _, 3, 4)(1, _)(2, 5)).toBe('1_2_3');
+  });
+  test('test #6', () => {
+    expect(curry(join)(_, _, _, _)(_, 2, _)(_, 3)(1)).toBe('1_2_3');
+  });
+  test('test #7', () => {
+    expect(curry(join)(1)(_, 3)(2)).toBe('1_2_3');
   });
 });
