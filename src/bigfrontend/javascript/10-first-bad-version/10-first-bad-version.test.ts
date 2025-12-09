7e7d8a8a5909fa366cbcd802cab6022306818886
Max Zaytsev <zaytsev.mxm@gmail.com>
2025-12-09 23:11:27 +0000

    Fixed version

--- src/bigfrontend/javascript/10-first-bad-version/10-first-bad-version.test.ts
+++ src/bigfrontend/javascript/10-first-bad-version/10-first-bad-version.test.ts
@@ -3,6 +3,10 @@ import { firstBadVersion } from './10-first-bad-version';
 
 describe('firstBadVersion', () => {
   test('it works correctly in all cases', () => {
-    expect(firstBadVersion((n) => n > 7)(10)).toEqual(8);
+    expect(firstBadVersion((i) => i >= 4)(100)).toEqual(4);
+    expect(firstBadVersion((i) => i >= 4)(4)).toEqual(4);
+    expect(firstBadVersion((i) => i >= 5)(3)).toEqual(-1);
+    expect(firstBadVersion((i) => i >= 1)(1)).toEqual(1);
+    expect(firstBadVersion((i) => i >= 1)(2)).toEqual(1);
   });
 });
--- src/bigfrontend/javascript/10-first-bad-version/10-first-bad-version.ts
+++ src/bigfrontend/javascript/10-first-bad-version/10-first-bad-version.ts
@@ -5,12 +5,12 @@ type IsBad = (version: number) => boolean;
 function firstBadVersion(isBad: IsBad) {
   // firstBadVersion receive a check function isBad
   // and should return a closure which accepts a version number(integer)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return (_version: number): number => {
+
+  return (version: number): number => {
     // write your code to return the first bad version
     // if none found, return -1
     let left = 0;
-    let right = Number.MAX_SAFE_INTEGER;
+    let right = version;
 
     while (left < right) {
       const pivot = Math.floor((left + right) / 2);
