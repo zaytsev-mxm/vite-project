c9a79b877089e70a8af14eb4e90272bb0bc84749
Max Zaytsev <zaytsev.mxm@gmail.com>
2026-01-08 18:53:28 +0000

    Add quickSort tests and add some improvements to the quickSort itself

--- src/utils/01-quick-sort/01-quick-sort.ts
+++ src/utils/01-quick-sort/01-quick-sort.ts
@@ -1,9 +1,9 @@
 const quickSort = (nums: number[]): number[] => {
-  if (nums.length < 2) return nums;
+  if (nums.length < 2) return [...nums];
 
   const pivot = Math.floor(nums.length / 2);
-  const smaller = [];
-  const larger = [];
+  const smaller: number[] = [];
+  const larger: number[] = [];
   for (let i = 0; i < nums.length; i++) {
     if (i === pivot) continue;
     if (nums[i] <= nums[pivot]) {
@@ -12,7 +12,7 @@ const quickSort = (nums: number[]): number[] => {
       larger.push(nums[i]);
     }
   }
-  return [...quickSort(smaller), ...[nums[pivot]], ...quickSort(larger)];
+  return [...quickSort(smaller), nums[pivot], ...quickSort(larger)];
 };
 
 export { quickSort };
