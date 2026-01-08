501e7c6a08730c677ac2f2ea9354b42c1b40f39d
Max Zaytsev <zaytsev.mxm@gmail.com>
2026-01-08 18:43:27 +0000

    Add quickSort

--- src/utils/01-quick-sort/01-quick-sort.ts
+++ src/utils/01-quick-sort/01-quick-sort.ts
@@ -1,3 +1,18 @@
-const quickSort = () => {};
+const quickSort = (nums: number[]): number[] => {
+  if (nums.length < 2) return nums;
+
+  const pivot = Math.floor(nums.length / 2);
+  const smaller = [];
+  const larger = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i === pivot) continue;
+    if (nums[i] <= nums[pivot]) {
+      smaller.push(nums[i]);
+    } else {
+      larger.push(nums[i]);
+    }
+  }
+  return [...quickSort(smaller), ...[nums[pivot]], ...quickSort(larger)];
+};
 
 export { quickSort };
