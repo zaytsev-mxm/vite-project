2812953d5910f1d5b074c3f3b44e8bb2863d5edf
Max Zaytsev <zaytsev.mxm@gmail.com>
2026-01-09 12:12:23 +0000

    Added a more real-world quickSort

--- src/utils/01-quick-sort/01-quick-sort.ts
+++ src/utils/01-quick-sort/01-quick-sort.ts
@@ -1,4 +1,4 @@
-const quickSort = (nums: number[]): number[] => {
+const quickSortNaive = (nums: number[]): number[] => {
   if (nums.length < 2) return [...nums];
 
   const pivot = Math.floor(nums.length / 2);
@@ -12,7 +12,58 @@ const quickSort = (nums: number[]): number[] => {
       larger.push(nums[i]);
     }
   }
-  return [...quickSort(smaller), nums[pivot], ...quickSort(larger)];
+  return [...quickSortNaive(smaller), nums[pivot], ...quickSortNaive(larger)];
 };
 
-export { quickSort };
+// partition function
+function partition(arr: number[], low: number, high: number) {
+  // choose the pivot
+  const pivot = arr[high];
+
+  // index of smaller element and indicates
+  // the right position of pivot found so far
+  let i = low - 1;
+
+  // traverse arr[low..high] and move all smaller
+  // elements to the left side. Elements from low to
+  // i are smaller after every iteration
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      swap(arr, i, j);
+    }
+  }
+
+  // move pivot after smaller elements and
+  // return its position
+  swap(arr, i + 1, high);
+  return i + 1;
+}
+
+// swap function
+function swap(arr: number[], i: number, j: number) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+// the QuickSort function implementation
+function quickSortHelper(arr: number[], low: number, high: number) {
+  if (low < high) {
+    // pi is the partition return index of pivot
+    const pi = partition(arr, low, high);
+
+    // recursion calls for smaller elements
+    // and greater or equals elements
+    quickSortHelper(arr, low, pi - 1);
+    quickSortHelper(arr, pi + 1, high);
+  }
+  return arr;
+}
+
+const quickSort = (_arr: number[]) => {
+  const arr = _arr.slice();
+  return quickSortHelper(arr, 0, arr.length - 1);
+};
+
+export { quickSortNaive, quickSort };
