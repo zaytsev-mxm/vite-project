aba6f3ad5bf9a8c1b1943c23dfaf6d1a25adb6f4
Max Zaytsev <zaytsev.mxm@gmail.com>
2026-02-23 22:32:44 +0000

    Add returning indexes for the O(n) solution

--- src/utils/05-get-max-sub-sum/05-get-max-sub-sum.test.ts
+++ src/utils/05-get-max-sub-sum/05-get-max-sub-sum.test.ts
@@ -1,5 +1,9 @@
 import { describe, test, expect } from 'vitest';
-import { getMaxSubSum, getMaxSubSumOn } from './05-get-max-sub-sum';
+import {
+  getMaxSubSum,
+  getMaxSubSumOn,
+  getMaxSubSumOnWithIndexes,
+} from './05-get-max-sub-sum';
 
 describe('Get max. sub-array sum', () => {
   describe('getMaxSubSum', () => {
@@ -25,6 +29,25 @@ describe('Get max. sub-array sum', () => {
       expect(getMaxSubSumOn([1, 2, 3])).eq(6); // [1, 2, 3]
       expect(getMaxSubSumOn([-1, -2, -3])).eq(-1); // [-1]
       expect(getMaxSubSum([])).eq(0); // []
+    });
+  });
+
+  describe('getMaxSubSumOnWithIndexes', () => {
+    test('it works correctly', () => {
+      expect(getMaxSubSumOnWithIndexes([-1, 2, 3, -9])).toEqual([5, [1, 2]]); // [2, 3]
+      expect(getMaxSubSumOnWithIndexes([2, -1, 2, 3, -9])).toEqual([6, [0, 3]]); // [2, -1, 2, 3]
+      expect(getMaxSubSumOnWithIndexes([-1, 2, 3, -9, 11])).toEqual([
+        11,
+        [4, 4],
+      ]); // [11]
+      expect(getMaxSubSumOnWithIndexes([-2, -1, 1, 2])).toEqual([3, [2, 3]]); // [1, 2]
+      expect(getMaxSubSumOnWithIndexes([100, -9, 2, -3, 5])).toEqual([
+        100,
+        [0, 0],
+      ]); // [100]
+      expect(getMaxSubSumOnWithIndexes([1, 2, 3])).toEqual([6, [0, 2]]); // [1, 2, 3]
+      expect(getMaxSubSumOnWithIndexes([-1, -2, -3])).toEqual([-1, [0, 0]]); // [-1]
+      expect(getMaxSubSumOnWithIndexes([])).toEqual([0, [0, 0]]); // []
     });
   });
 });
--- src/utils/05-get-max-sub-sum/05-get-max-sub-sum.ts
+++ src/utils/05-get-max-sub-sum/05-get-max-sub-sum.ts
@@ -26,3 +26,34 @@ export const getMaxSubSumOn = (nums: number[]): number => {
 
   return maxSum;
 };
+
+export const getMaxSubSumOnWithIndexes = (
+  nums: number[],
+): [number, [number, number]] => {
+  if (!nums.length) return [0, [0, 0]];
+  let maxSum = -Infinity;
+  let partialSum = 0;
+  let start = 0;
+  let tempStart = 0;
+  let end = 0;
+  let i = 0;
+
+  for (const num of nums) {
+    partialSum += num;
+
+    if (partialSum > maxSum) {
+      maxSum = partialSum;
+      start = tempStart;
+      end = i;
+    }
+
+    if (partialSum < 0) {
+      partialSum = 0;
+      tempStart = i + 1;
+    }
+
+    i++;
+  }
+
+  return [maxSum, [start, end]];
+};
